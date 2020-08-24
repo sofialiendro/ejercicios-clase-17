0d812d95d294dfc1331d729024352a49d310f65c
Sofia Liendro <sofialiendro.s@gmail.com>
2020-08-24 00:01:35 -0300

    Agrego ejercicios de JS

--- index.js
+++ index.js
@@ -30,3 +30,33 @@ console.log(esFraccionMayorAUno(4/2)) // true
 
 
 // Ejercicio 3
+
+const aHackerSpeak = (str) => {
+    let frasesAMinusculas = str.toLowerCase()
+    let reemplazoLaI = frasesAMinusculas.replace(/i/g, "1")
+    let reemplazoLaE = reemplazoLaI.replace(/e/g, "3")
+    let reemplazoLaA = reemplazoLaE.replace(/a/g, "4")
+    let reemplazoLaS = reemplazoLaA.replace(/s/g, "5")
+    let reemplazoLaO = (letrasReemplazadas = reemplazoLaS.replace(/o/g, "0"))
+    return letrasReemplazadas
+}
+
+console.log(aHackerSpeak('javascript')) // 'j4v45cr1pt'
+console.log(aHackerSpeak('soy una hacker')) // '50y un4 h4ck3r'
+console.log(aHackerSpeak('ADA LOVELACE')) // '4D4 L0V3L4C3'
+
+
+// Ejercicio 4
+
+const esPuenteSeguro = (puente) => {
+    if (puente.includes(" ")) {
+        return false
+    }
+    else {
+        return true
+    }
+}
+
+console.log(esPuenteSeguro('### ##')) // false
+console.log(esPuenteSeguro('##### ####')) // false
+console.log(esPuenteSeguro('########')) // true
